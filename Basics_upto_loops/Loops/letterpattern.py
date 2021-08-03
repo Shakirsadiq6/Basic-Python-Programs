2aea264d61acf0704b0bb66cf0ba936925d30cb9
Shakir Sadiq <80309757+Shakirsadiq6@users.noreply.github.com>
2021-08-03 15:17:03 +0530

    Update letterpattern.py

--- Basics_upto_loops/Loops/letterpattern.py
+++ Basics_upto_loops/Loops/letterpattern.py
@@ -1,5 +1,5 @@
 '''Program to produce the following output
-     A B C D E F G F E D C B A
+         A B C D E F G F E D C B A
 	 A B C D E F   F E D C B A
 	 A B C D E       E D C B A
 	 A B C D           D C B A
